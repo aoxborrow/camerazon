4ca01c3b86747b5a2724ff14045fa8c35f039e7e
Aaron Oxborrow <aaron@pastedesign.com>
2013-07-11 19:43:34 -0700

    initial commit, add composer and shopify api lib

---
+++ README.md
@@ -0,0 +1,4 @@
+# Camazon
+### The 2nd Best Place to Buy Cameras Online
+
+Test project for Stitch Labs.
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// setup error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+// composer autoload
+require 'vendor/autoload.php';
+
+// shopify api lib
+use sandeepshetty\shopify_api;
+
+echo "Hello World.";
